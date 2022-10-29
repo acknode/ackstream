78e644c1dc40445d2b1ba5803d2cd02e64349a19
Tuan Nguyen <tuan.nguyen@acknode.com>
2022-10-29 09:23:18 +0700

    refactor: should use simple setup for local development with docker

--- scripts/docker-compose-reset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker compose -p ackstream -f docker-compose/docker-compose.yaml down
-docker volume prune -f
-docker compose -p ackstream -f docker-compose/docker-compose.yaml up -d
